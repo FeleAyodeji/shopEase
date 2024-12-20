3ea602d967d99aeb21c3f6ff13e856039e2407bf
FeleAyodeji <feledeji@gmail.com>
2024-12-20 18:01:15 +0100

    added responive product listing , implemeted search bar , built cart functionality with the add and remove  feature and also enhanced the UI using tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import { useState } from "react";
+import ProductList from "./Components/ProductList";
+import SearchBar from "./Components/SearchBar";
+import Cart from "./Components/Cart";
+import { CartProvider } from "./context/CartContext";
+
 function App() {
-  return <div></div>;
+  const [searchQuery, setSearchQuery] = useState("");
+
+  return (
+    <CartProvider>
+      <div className="container mx-auto p-4">
+        <SearchBar setSearchQuery={setSearchQuery} />
+        <ProductList searchQuery={searchQuery} />
+        <Cart />
+      </div>
+    </CartProvider>
+  );
 }
 
 export default App;
